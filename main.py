e1e57fb635ec2701feb86c89f70872948f024966
Hayyan67 <humairabaig81@gmail.com>
2024-09-20 17:59:31 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+n=input('Enter a number')
+if n%20==0:
+    print('twist')
+if n%15==0:
+    pass
+    if n%5==0:
+    print('Fizz')
+if n%20==0:
+    print('twist')
+if n%3==0:
+    print('buzz')
+else:
+    print(n)
